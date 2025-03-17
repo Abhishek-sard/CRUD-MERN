7e9afedc44c90dfae62cb550b63d2f64f82afbfb
Abhishek Sardar <143304955+Abhishek-sard@users.noreply.github.com>
2025-03-17 21:30:18 +0545

    Client

---
+++ src/App.jsx
@@ -0,0 +1,20 @@
+import './App.css'
+import { BrowserRouter, Routes, Route } from 'react-router-dom'
+import 'bootstrap/dist/css/bootstrap.min.css'
+import UpdateUser from './UpdateUser'
+import Users from './Users'
+import CreateUser from './CreateUser'
+
+function App() {
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route path='/' element={<Users />} />
+        <Route path='/create' element={<CreateUser />} />
+        <Route path='/update' element={<UpdateUser />} />
+      </Routes>
+    </BrowserRouter>
+  )
+}
+
+export default App
